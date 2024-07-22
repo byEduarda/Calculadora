f4e707b613f2c54a9e0bb21f7ec3b23114557aa0
Maria Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2024-07-21 21:03:29 -0300

    .

--- script.js
+++ script.js
@@ -1,78 +1,82 @@
 const currDisplay = document.querySelector(".curr-display")
 const prevDisplay = document.querySelector(".prev-display")
-const numbers = document.querySelector(".number")
-const operands = document.querySelector(".operation")
+const numbers = document.querySelectorAll(".number")
+const operands = document.querySelectorAll(".operation")
 const clearBtn = document.querySelector(".clear")
 const delBtn = document.querySelector(".delete")
 const equalBtn = document.querySelector(".equal")
-let operation
+let operation = null;
 
 function appendNumber(number) {
-    if (number == "." && currDisplay.innerHTML.includes("."))return
-    currDisplay.innerHTML += number
+    if (number === "." && currDisplay.innerHTML.includes(".")) return;
+    currDisplay.innerText += number;
 }
 
 function chooseOperation(operand) {
-    if (currDisplay.innerHTML == "") return
-    compute(operand)
-    operation = operand
-    currDisplay.innerHTML += operand
-    prevDisplay.innerHTML = currDisplay.innerText
-    currDisplay.innerHTML = ""
+    if (currDisplay.innerHTML === "") return;
+    if (prevDisplay.innerHTML !== "") {
+        compute(operand);
+    }
+    
+    operation = operand;
+    currDisplay.innerHTML += operand;
+    prevDisplay.innerHTML = currDisplay.innerText + operand;
+    currDisplay.innerHTML = "";
 }
 
 function clearDisplay() {
-    currDisplay.innerHTML = ""
-    prevDisplay.innerHTML = ""
+    currDisplay.innerHTML = "";
+    prevDisplay.innerHTML = "";
+    operation = null;
 }
 
 function compute(operand) {
-    let result
-    const previousValue = parseFloat(prevDisplay.innerHTML)
-    const currentValue = parseFloat(currDisplay.innerHTML)
+    let result;
+    const previousValue = parseFloat(prevDisplay.innerHTML);
+    const currentValue = parseFloat(currDisplay.innerHTML);
 
-    if (isNaN(previousValue) || isNaN(currentValue)) return
+    if (isNaN(previousValue) || isNaN(currentValue)) return;
     
     switch (operation) {
         case "+":
             result = previousValue + currentValue;
-            break
+            break;
         case "-":
             result = previousValue - currentValue;
-            break
+            break;
         case "*":
             result = previousValue * currentValue;
-            break
+            break;
         case "/":
             result = previousValue / currentValue;
-            break
+            break;
         default:
-            return
+            return;
     }
-    currDisplay.innerHTML = result
+    currDisplay.innerHTML = result;
 }
 
 numbers.forEach((number) => {
     number.addEventListener("click", () => {
         appendNumber(number.innerHTML);
-    })
-})
+    });
+});
 
 operands.forEach((operand) => {
     operand.addEventListener("click", () => {
         chooseOperation(operand.innerHTML);
-    })
-})
+    });
+});
 
 clearBtn.addEventListener("click", () => {
-    clearDisplay()
-})
+    clearDisplay();
+});
 
 equalBtn.addEventListener("click", () => {
     compute();
-    prevDisplay.innerHTML = ""
-})
+    prevDisplay.innerHTML = "";
+});
 
 delBtn.addEventListener("click", () => {
     currDisplay.innerHTML = currDisplay.innerHTML.slice(0, -1);
-})
+});
